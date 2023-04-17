ebe9d4abf64e3e6aba3be110f7d7e7c63b1d8526
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-04-17 23:00:07 +0300

    feat: add few tasks  (Monday, April 17, 2023 8:00:07 PM)

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,29 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(newDate) {
+  let month = newDate.getMonth();
+  if (month < 2 || month == 11) {
+    return 'winter';
+  } else if (month < 5) {
+    return 'spring';
+  } else if (month < 8) {
+    return 'summer';
+  } else if (month < 11) {
+    return 'autumn';
+  }
   // remove line with error and write your code here
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
